8b5bff8d0af0305009d013e72edb024a35fd7bf1
Kenji Ozawa <keoz002@ess.ais.ac.nz>
2025-04-15 01:43:37 +0000

    🚀 Auto-deploy full project to stage (2025-04-15 01:43)

--- target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/src/Components/Layout/header.js
+++ target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/target-repo/src/Components/Layout/header.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import './header.css';
 
 const Header: React.FC = () => {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    setToken(localStorage.getItem("Token"));
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("Token");
+    setToken(null);
+    window.location.href = "/login";
+  };
   return (
     <header className="header">
       <div className="header-container">
@@ -10,9 +21,15 @@ const Header: React.FC = () => {
           <Link to="/" className="nav-link">Home</Link>
           <Link to="/posts" className="nav-link">Posts</Link>
         </nav>
-        <div className="nav-right">
-          <Link to="/login" className="btn btn-login">Login</Link>
-        </div>
+        {token ? (
+                 <>
+                    <li className="nav-right"><a href="/login" onClick={(e) => { e.preventDefault(); handleLogout(); }} className="btn btn-login">Logout</a></li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-right"><Link to="/register" className="btn btn-login">Register</Link>&nbsp;<Link to="/login" className="btn btn-login">Login</Link></li>
+                </>
+        )}
       </div>
     </header>
   );
